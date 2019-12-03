7ec222e53e1accd79d1dc24d9bdb9ffb7cb546cf
Kyle <kylepcole@gmail.com>
2019-12-03 12:39:49 -0500

    lab working, last test not passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # def cart
+  # 	@cart = current_cart
+  # end
+
+  def current_cart
+    @cart = session[:cart] ||= []
+  end
+
+  def add_to_cart
+	current_cart << params[:product]
+	# byebug
+	redirect_to products_path
+	# redirect_to homepage_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :products, only: [:index]
+  # post '/items/:id/add_to_cart', to: 'application#add_to_cart', as: "add_to_cart"
+  post '/add_to_cart', to: 'application#add_to_cart', as: "add_to_cart"
+  get '/', to: 'products#index', as: 'homepage'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
